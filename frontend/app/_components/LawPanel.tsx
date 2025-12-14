097572b50dc1ab1482bfc8d48c94d120e4a7ce05
tkms1 <f.river.in.port@gmail.com>
2025-12-14 20:07:17 +0900

    fix: 本番環境のURL習性

--- frontend/app/_components/LawPanel.tsx
+++ frontend/app/_components/LawPanel.tsx
@@ -150,7 +150,9 @@ export const LawPanel: React.FC = () => {
       const lawId = foundButton.link.replace(/^\//, "");
       setSelectedLawId(lawId);
       try {
-        const res = await fetch(`http://localhost:3000/api?lawId=${lawId}`);
+        const res = await fetch(
+          `https://law-cbt.vercel.app/api?lawId=${lawId}`
+        );
         if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
         const json = await res.json();
         setLawData(json);
